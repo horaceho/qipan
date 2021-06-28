0596c258b804ba08754f8ee184f834300e63a280
Horace Ho <horaceh@gmail.com>
2021-06-28 21:39:22 +0800

    show board

--- src/components/Pan.vue
+++ src/components/Pan.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="mx-auto px-4">
-    <div class="space-y-6">
+    <div class="text-center space-y-6 mb-6">
       <div class="text-lg leading-6 font-medium text-gray-900">FEN {{ xiang.version() }}</div>
-      <p class="mt-2 text-sm text-gray-500">{{ fen }}</p>
+      <p class="mt-2 text-xs text-gray-500">{{ fen }}</p>
     </div>
-    <div>
-      <button class="mx-1" type="button" @click="count--">－</button>
-      {{ count }}
-      <button class="mx-1" type="button" @click="count++">＋</button>
-    </div>
-    <div class="grid grid-cols-9 gap-4">
+
+    <div class="max-w-sm mx-auto grid grid-cols-9 gap-4">
       <div v-for="(grid) in xiang.grids()" :key="grid.index">
-        {{ grid.chi }}
+        <div class="text-3xl">{{ grid.chi }}</div>
       </div>
+    </div>
+
+    <div class="text-center text-lg mt-6">
+        <button class="mx-1" type="button" @click="count--">－</button>
+        {{ count }}
+        <button class="mx-1" type="button" @click="count++">＋</button>
     </div>
   </div>
 </template>
